cb61f401c9d72928f651ab02f8668f8b1facb8f8
Kristina Krajevska <krajevska.kristina98@gmail.com>
2018-05-13 20:40:47 +0200

    IsTouching() method added.

--- SheepInTheCity/Form2.Designer.cs
+++ SheepInTheCity/Form2.Designer.cs
@@ -30,19 +30,32 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(194, 414);
+            this.pictureBox1.Location = new System.Drawing.Point(187, 414);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(28, 37);
+            this.pictureBox1.Size = new System.Drawing.Size(30, 39);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(187, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
             // Form2
             // 
@@ -51,6 +64,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(419, 493);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -58,6 +72,7 @@
             this.Text = "Sheep In The City";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -65,5 +80,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- SheepInTheCity/Form2.cs
+++ SheepInTheCity/Form2.cs
@@ -10,8 +10,11 @@ using System.Windows.Forms;
 
 namespace SheepInTheCity
 {
+
     public partial class Form2 : Form
     {
+
+        private Form5 form5;
         public Form2()
         {
             KeyDown += new KeyEventHandler(Form2_KeyDown);
@@ -22,11 +25,58 @@ namespace SheepInTheCity
         {
             int X = pictureBox1.Location.X;
             int Y = pictureBox1.Location.Y;
-            if (e.KeyCode == Keys.Right) X += 5;
-            else if (e.KeyCode == Keys.Left) X -= 5;
-            else if (e.KeyCode == Keys.Up) Y -= 5;
-            else if (e.KeyCode == Keys.Down) Y += 5;
+            if (e.KeyCode == Keys.Right)
+            {
+                if (pictureBox1.Location.X <= 377) {
+                    X += 5;
+                }
+               
+            }
+            else if (e.KeyCode == Keys.Left) 
+            {
+                if (pictureBox1.Location.X >= 15)
+                {
+                    X -= 5;
+                }
+            }
+            else if (e.KeyCode == Keys.Up) 
+            {
+                if(pictureBox1.Location.Y>=2)
+                {
+                    Y -= 5;
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (pictureBox1.Location.Y <=414)
+                {
+                    Y+= 5;
+                }
+            }
+            
             pictureBox1.Location = new Point(X, Y);
+            if(IsTouching(pictureBox1,pictureBox2))
+            {
+                form5 = new Form5();
+                form5.Show();
+            }
+        }
+        private bool IsTouching(PictureBox p1, PictureBox p2)
+        {
+            if (p1.Location.X + p1.Width < p2.Location.X)
+                return false;
+            if (p2.Location.X + p2.Width < p1.Location.X)
+                return false;
+            if (p1.Location.Y + p1.Height < p2.Location.Y)
+                return false;
+            if (p2.Location.Y + p2.Height < p1.Location.Y)
+                return false;
+            return true;
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
